00ce9760e47249c64360052ce5ad7f1a5af9c4ff
xhoch3.info <xhoch3.info@gmail.com>
2018-11-09 15:59:35 +0100

    Optimize RegEx Code

--- reg_ex.sql
+++ reg_ex.sql
@@ -1,24 +1,21 @@
 
---ich hätte da ein Statement zur Aufgabe mit den Vokalen aber mir wird selber schon langsam schwindelig.... ;)
-select first_name, last_name, length(to_char(salary)) sal_stellen, replace(regexp_replace(lower(first_name ||
 
-                    last_name), '[bcdfghjklmnpqrstvwxyz]', ''), ' ', '') vocale,
+select  first_name
+        , last_name
+        , length    (   to_char(salary) ) "FiguresSalary"
+        , regexp_replace(lower(first_name || last_name), '[bcdfghjklmnpqrstvwxyz]', '') "VocalsOnly"
+        , length    (   regexp_replace(lower(first_name || last_name), '[bcdfghjklmnpqrstvwxyz]', '')) "VocalsCount"
+from    employees e
+where   length(regexp_replace(lower(first_name || last_name), '[bcdfghjklmnpqrstvwxyz]', '')) 
+        >   (   select length(  to_char(i.salary)   ) 
+                from employees i
+                where i.employee_id = e.employee_id     ); 
+                                                                              
 
-        length(replace(regexp_replace(lower(first_name ||
-
-                    last_name), '[bcdfghjklmnpqrstvwxyz]', ''), ' ', '')) vocsnr
-
-from employees e
-
-where length(replace(regexp_replace(lower(first_name ||
-
-                    last_name), '[bcdfghjklmnpqrstvwxyz]', ''), ' ', '')) > (select length(to_char(i.salary)) from employees i
-
-                                                                              where i.employee_id = e.employee_id); 
-
-
-SELECT last_name, replace(regexp_replace(lower(first_name || last_name), '[bcdfghjklmnpqrstvwxyz]', ''), ' ', '') vocale, 
-regexp_replace(lower(first_name || last_name), '[bcdfghjklmnpqrstvwxyz]', '') schritt,
-length(replace(regexp_replace(lower(first_name ||last_name), '[bcdfghjklmnpqrstvwxyz]', ''), ' ', '')) vocsnr,
-LENGTH (regexp_replace(lower(first_name ||last_name), '[bcdfghjklmnpqrstvwxyz]', '')) schrittt2
-FROM employees ;
+SELECT  first_name||' '||last_name "Name"
+        , salary    "Gehalt"
+        , length    (   to_char(salary) ) "LängeGehalt"
+        , regexp_replace(lower(first_name || last_name), '[bcdfghjklmnpqrstvwxyz]', '') "Vokale"
+        , LENGTH (regexp_replace(lower(first_name ||last_name), '[bcdfghjklmnpqrstvwxyz]', '')) "Anzahl"
+FROM employees
+where length(  to_char(salary) ) < LENGTH (regexp_replace(lower(first_name ||last_name), '[bcdfghjklmnpqrstvwxyz]', ''));
