a3a465890176bbe6e94c81964dbc09069526b481
Dieter Reimann <dieter.reimann@gmail.com>
2018-11-13 13:24:39 +0100

    sync from comcave

--- ora1.sql
+++ ora1.sql
@@ -1,1 +1,11 @@
-select * from departments
+select * from big_table where object_name = 'EMPLOYEES';
+
+create index big_table_obj_name_idx on big_table(object_name);
+create index big_table_obj_name_idx2 on big_table(lower(object_name));
+create bitmap index big_table_obj_name_idx1 on big_table(object_name);
+
+
+drop index big_table_obj_name_idx;
+drop index big_table_obj_name_idx2;
+
+select * from big_table where object_name = 'EMPLOYEES';
