1b96db72314a93179d878245930dbb36f93b7748
Dieter Reimann <dieter.reimann@gmail.com>
2018-12-04 11:24:14 +0100

    Initializing Repo

---
+++ PL_SQL/OracleJET/newSQLTemplate.sql
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  dreimann
+ * Created: 04.12.2018
+ */
+@C:\app\gitSQL\PL_SQL\login.sql;
+
+SELECT * FROM ORA21.COUNTRIES WHERE ROWNUM <= 1000;
